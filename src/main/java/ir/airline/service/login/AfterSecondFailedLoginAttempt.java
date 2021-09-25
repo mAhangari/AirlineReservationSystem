f060eff217ec0f6ef1b6a1a7e7370506e9bbd4e6
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-26 02:08:46 +0330

    Add AfterSecondFailedLoginAttempt

---
+++ src/main/java/ir/airline/service/login/AfterSecondFailedLoginAttempt.java
@@ -0,0 +1,25 @@
+package ir.airline.service.login;
+
+import ir.airline.domain.User;
+import ir.airline.util.ApplicationContext;
+import ir.airline.util.LoginServiceContext;
+
+public class AfterSecondFailedLoginAttempt extends LoginServiceState {
+    private final String previousAccountId;
+
+    public AfterSecondFailedLoginAttempt(String previousAccountId) {
+        this.previousAccountId = previousAccountId;
+    }
+
+    @Override
+    public void handleIncorrectPassword(LoginServiceContext context, User account,
+                                        String password) {
+        if (previousAccountId.equals(account.getUsername())) {
+            account.setRevoked(true);
+            ApplicationContext.userServ.save(account);
+            context.setState(new AwaitingFirstLoginAttempt());
+        } else {
+            context.setState(new AfterFirstFailedLoginAttempt(account.getUsername()));
+        }
+    }
+}
