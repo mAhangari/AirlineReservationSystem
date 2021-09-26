f098c5c11e658e5f78b46bda7f0c46bb9acca18f
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-26 23:02:43 +0330

    Add Menu

---
+++ src/main/java/ir/airline/service/abstractFactoryMenu/Menu.java
@@ -0,0 +1,58 @@
+package ir.airline.service.abstractFactoryMenu;
+
+import ir.airline.domain.User;
+import ir.airline.service.abstractFactoryMenu.impl.ClientApp;
+import ir.airline.util.ApplicationContext;
+import java.util.*;
+
+public class Menu extends BaseMenu {
+
+    Scanner input = new Scanner(System.in);
+
+    public void showMenu() {
+        try {
+            List<String> list = new ArrayList<>(Arrays.asList("Login User", "Sign Up", "Exit"));
+            optionMessage(list, true);
+
+            switch (input.nextInt()) {
+                case 1 -> {
+                    BaseMenu.singleSetMessage("Insert your Username");
+                    var username = input.next();
+
+                    BaseMenu.singleSetMessage("Insert your Password");
+                    var password = input.next();
+
+                    User account = ApplicationContext.loginServ.login(username, password);
+
+                    if (account.isLoggedIn()) {
+                        ApplicationContext.userServ.save(account);
+                        BaseMenu.singlePrintMessage(BaseMenu.WELCOME);
+
+                        ClientApp app = ClientAppConfiguration.configureApp(account);
+                        ProfileMenu profile = app.getProfileMenu();
+
+                        profile.dashboard();
+                        account.setLoggedIn(false);
+                        ApplicationContext.userServ.save(account);
+                    }
+                    showMenu();
+                }
+                case 2 -> {
+                    ApplicationContext.customerServ.signUp();
+                    showMenu();
+                }
+
+                case 3 -> {
+                }
+                default -> {
+                    singlePrintMessage(WRONG_NUMBER);
+                    showMenu();
+                }
+            }
+        } catch (InputMismatchException e) {
+            input.nextLine();
+            singlePrintMessage(WRONG_NUMBER);
+            showMenu();
+        }
+    }
+}
