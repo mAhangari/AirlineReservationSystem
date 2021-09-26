672a229936befa5afbbcaa24cd4bc04be238da50
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-27 02:10:40 +0330

    Add FlightScheduleService

---
+++ src/main/java/ir/airline/service/FlightScheduleService.java
@@ -0,0 +1,7 @@
+package ir.airline.service;
+
+import ir.airline.domain.FlightSchedule;
+import ir.baseCRUD.base.service.BaseEntityService;
+
+public interface FlightScheduleService extends BaseEntityService<FlightSchedule, Long> {
+}
