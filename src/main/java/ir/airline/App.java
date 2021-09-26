b9859a634abed092fdcf029936c4f1a628607481
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-26 17:12:20 +0330

    Add App

---
+++ src/main/java/ir/airline/App.java
@@ -0,0 +1,9 @@
+package ir.airline;
+
+import ir.airline.util.ApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext.menu.showMenu();
+    }
+}
