3f7ad6485ecee788d84da0badf67437014bb179c
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-27 02:10:25 +0330

    Add FlightScheduleRepository

---
+++ src/main/java/ir/airline/repository/FlightScheduleRepository.java
@@ -0,0 +1,7 @@
+package ir.airline.repository;
+
+import ir.airline.domain.FlightSchedule;
+import ir.baseCRUD.base.repository.BaseEntityRepository;
+
+public interface FlightScheduleRepository extends BaseEntityRepository<FlightSchedule, Long> {
+}
