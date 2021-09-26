2af613b9f5ac7958c521eadccbac55199c9b9aa1
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-27 02:10:11 +0330

    Modify FlightSchedule add ManyToOne relationship to AirLine

--- src/main/java/ir/airline/domain/FlightSchedule.java
+++ src/main/java/ir/airline/domain/FlightSchedule.java
@@ -7,8 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Entity
@@ -27,8 +28,11 @@ public class FlightSchedule extends BaseEntity<Long> {
 
     private String source;
 
-    private LocalDate flyTime;
+    private LocalDateTime flyTime;
 
     @OneToMany(mappedBy = "flightSchedule")
     private Set<Ticket> ticket;
+
+    @ManyToOne
+    private Airline airline;
 }
