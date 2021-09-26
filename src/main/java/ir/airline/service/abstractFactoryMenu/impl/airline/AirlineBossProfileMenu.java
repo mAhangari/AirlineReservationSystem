f57698d5bc6098e5cd176102111d759f5adbd285
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-26 22:58:24 +0330

    Add AirlineBossProfileMenu

---
+++ src/main/java/ir/airline/service/abstractFactoryMenu/impl/airline/AirlineBossProfileMenu.java
@@ -0,0 +1,47 @@
+package ir.airline.service.abstractFactoryMenu.impl.airline;
+
+import ir.airline.domain.Employee;
+import ir.airline.service.abstractFactoryMenu.BaseMenu;
+import ir.airline.service.abstractFactoryMenu.ProfileMenu;
+import ir.airline.util.ApplicationContext;
+import lombok.AccessLevel;
+import lombok.Getter;
+import java.util.*;
+
+public class AirlineBossProfileMenu implements ProfileMenu {
+
+    Scanner input = new Scanner(System.in);
+
+    @Getter(AccessLevel.PRIVATE)
+    private final Employee employee;
+
+    public AirlineBossProfileMenu(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public void dashboard() {
+        try {
+            List<String> list = new ArrayList<>(
+                    Arrays.asList("Crate new Employee", "Log Out"));
+            BaseMenu.optionMessage(list, true);
+
+            switch (input.nextInt()) {
+                case 1 -> {
+                    ApplicationContext.employeeServ.signUp();
+                    dashboard();
+                }
+                case 2 -> {
+                }
+                default -> {
+                    BaseMenu.singlePrintMessage(BaseMenu.WRONG_NUMBER);
+                    dashboard();
+                }
+            }
+        } catch (InputMismatchException e) {
+            input.nextLine();
+            BaseMenu.singlePrintMessage(BaseMenu.WRONG_NUMBER);
+            dashboard();
+        }
+    }
+}
