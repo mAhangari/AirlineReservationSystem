edb356155716762833e2918cb621e9313e0a77ee
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-27 20:14:58 +0330

    Modify FlightScheduleRepository add findAllBySourceAndDestination method

--- src/main/java/ir/airline/repository/FlightScheduleRepository.java
+++ src/main/java/ir/airline/repository/FlightScheduleRepository.java
@@ -2,6 +2,9 @@ package ir.airline.repository;
 
 import ir.airline.domain.FlightSchedule;
 import ir.baseCRUD.base.repository.BaseEntityRepository;
+import java.util.List;
 
 public interface FlightScheduleRepository extends BaseEntityRepository<FlightSchedule, Long> {
+
+    List<FlightSchedule> findAllBySourceAndDestination(String source, String destination);
 }
