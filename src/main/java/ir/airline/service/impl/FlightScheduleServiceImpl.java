418b5d360b1262ad20c26f0c45f3ad302b28b4fa
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-27 20:16:12 +0330

    Modify FlightScheduleServiceImpl add findAllBySourceAndDestination method

--- src/main/java/ir/airline/service/impl/FlightScheduleServiceImpl.java
+++ src/main/java/ir/airline/service/impl/FlightScheduleServiceImpl.java
@@ -4,11 +4,26 @@ import ir.airline.domain.FlightSchedule;
 import ir.airline.repository.FlightScheduleRepository;
 import ir.airline.service.FlightScheduleService;
 import ir.baseCRUD.base.service.impl.BaseEntityServiceImpl;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import java.util.List;
 
 public class FlightScheduleServiceImpl extends BaseEntityServiceImpl<FlightSchedule, Long, FlightScheduleRepository>
                         implements FlightScheduleService {
 
     public FlightScheduleServiceImpl(FlightScheduleRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public List<FlightSchedule> findAllBySourceAndDestination(String source, String destination) {
+        EntityManager em = repository.getEntityManager();
+        try {
+            return repository.findAllBySourceAndDestination(source, destination);
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            em.close();
+        }
     }
 }
