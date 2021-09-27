952435cd4b1682e10875f248849c55eb75f0cbcc
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-27 20:15:29 +0330

    Modify FlightScheduleService add findAllBySourceAndDestination method

--- src/main/java/ir/airline/service/FlightScheduleService.java
+++ src/main/java/ir/airline/service/FlightScheduleService.java
@@ -2,6 +2,9 @@ package ir.airline.service;
 
 import ir.airline.domain.FlightSchedule;
 import ir.baseCRUD.base.service.BaseEntityService;
+import java.util.List;
 
 public interface FlightScheduleService extends BaseEntityService<FlightSchedule, Long> {
+
+    List<FlightSchedule> findAllBySourceAndDestination(String source, String destination);
 }
